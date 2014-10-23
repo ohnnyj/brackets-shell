409e671b8e10a3e6298ffc52eb31297feab04829
john-smith <john.smith@am.sony.com>
2014-10-22 19:57:12 -0700

    update hack path

--- scripts/setup_for_hacking.js
+++ scripts/setup_for_hacking.js
@@ -2,7 +2,7 @@
 
 var exec = require('child_process').exec;
 var command = '../brackets/tools/setup_for_hacking.sh';
-var path = '/Volumes/Developer/src/github/scratch/brackets-shell/xcodebuild/Release/Brackets.app';
+var path = '/Volumes/Developer/src/github/ohnnyj/brackets-shell/xcodebuild/Release/Brackets.app';
 
 exec(command + ' ' + path, function(error, stdout, stderr) {
   stdout && console.log(stdout);
